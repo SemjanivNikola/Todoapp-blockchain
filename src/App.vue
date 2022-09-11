949c89a20d71456b3a8380523a5d723f851bbf70
Nikola <nikola.sem450@gmail.com>
2022-09-11 21:01:31 +0200

    Project simple init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <router-view />
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="css">
+#app {
+    height: 100vh;
+    background-color: var(--background-default);
+    overflow: hidden;
+}
+</style>

f75c0f04a9f43e9ae7c2741434af4ce0f27426e1
Nikola <nikola.sem450@gmail.com>
2022-09-18 14:28:17 +0200

    Added connection to metamask

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <template>
-    <router-view />
+    <div>
+        <div v-if="isLoading"><h1>Loading</h1></div>
+        <router-view v-else />
+    </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
     name: "App",
     data() {
         return {};
+    },
+    async created() {
+        await this.$store.dispatch("process");
+    },
+    computed: {
+        ...mapGetters(["isLoading"]),
     },
 };
 </script>
 
 <style lang="css">
 :root {
-	--primary: #EA40A4;
-	--business: #3A82EE;
-	--personal: var(--primary);
-	--light: #EEE;
-	--grey: #888;
-	--dark: #313154;
-	--danger: #ff5b57;
-	--shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
-	--business-glow: 0px 0px 4px rgba(58, 130, 238, 0.75);
-	--personal-glow: 0px 0px 4px rgba(234, 64, 164, 0.75);
+    --primary: #ea40a4;
+    --business: #3a82ee;
+    --personal: var(--primary);
+    --light: #eee;
+    --grey: #888;
+    --dark: #313154;
+    --danger: #ff5b57;
+    --shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+    --business-glow: 0px 0px 4px rgba(58, 130, 238, 0.75);
+    --personal-glow: 0px 0px 4px rgba(234, 64, 164, 0.75);
 }
 * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "montserrat", sans-serif;
 }
 #app {
     height: 100vh;
@@ -36,7 +47,7 @@ export default {
     overflow: hidden;
 }
 body {
-	background: var(--light);
-	color: var(--dark);
+    background: var(--light);
+    color: var(--dark);
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,76 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Web3 from 'web3'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    strict: process.env.NODE_ENV !== "production",
-    state: {},
-    mutations: {},
-    getters: {},
-    actions: {},
+  strict: process.env.NODE_ENV !== "production",
+  state: {
+    account: null,
+    web3Provider: null,
+    loading: false,
+  },
+  mutations: {
+    setProvider(state, payload) {
+      state.web3Provider = payload;
+    },
+    setAccount(state, payload) {
+      state.account = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    }
+  },
+  getters: {
+    isLoading(state) {
+      return state.loading;
+    },
+    getAccount(state) {
+      return state.account;
+    }
+  },
+  actions: {
+    async process({ dispatch, commit }) {
+      commit("setLoading", true);
+      try {
+        await dispatch("loadWeb3");
+        await dispatch("loadAccount");
+        commit("setLoading", false);
+      } catch (error) {
+        console.warn("Error on process: ", error);
+      }
+    },
+    async loadWeb3() {
+      if (window.ethereum) {
+        window.web3 = new Web3(ethereum)
+        // commit("setProvider", web3.currentProvider);
+        try {
+          // Request account access if needed
+          await ethereum.request({ method: 'eth_requestAccounts' });
+
+          const transactionHash = await ethereum.request({/* ... */ });
+          // Handle the result
+          console.log(transactionHash);
+        } catch (error) {
+          // User denied account access...
+        }
+      } else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      }
+    },
+    async loadAccount({ commit }) {
+      const accList = await ethereum.request({ method: 'eth_accounts' });
+      commit("setAccount", accList[0]);
+    },
+  },
+  // async loadContract() {
+  //   // Create a JavaScript version of the smart contract
+  //   const todoList = await $.getJSON('TodoList.json')
+  //   App.contracts.TodoList = TruffleContract(todoList)
+  //   App.contracts.TodoList.setProvider(App.web3Provider)
+
+  //   // Hydrate the smart contract with values from the blockchain
+  //   App.todoList = await App.contracts.TodoList.deployed()
+  // }
 });

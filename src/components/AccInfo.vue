3fbd6700744315657a76782b669d9be73445139f
Nikola <nikola.sem450@gmail.com>
2022-09-18 16:34:00 +0200

    Added AccInfo comp

---
+++ src/components/AccInfo.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="greeting">
+            <h2 class="title">Your address, {{getAccount}}</h2>
+        </section>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+    name: "AccInfo",
+    data() {
+        return {}
+    },
+    computed: {
+        ...mapGetters(['getAccount']),
+    }
+}
+</script>
+
+<style scoped>
+section {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5em;
+}
+.greeting .title {
+    display: flex;
+}
+.greeting .title {
+    color: var(--dark);
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+</style>
